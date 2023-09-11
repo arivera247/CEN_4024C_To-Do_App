093f241c136ea4ff2ef892bd1310623dbd765414
arivera247 <9136079+arivera247@users.noreply.github.com>
2023-09-10 21:43:09 -0400

    Created ArrayList variable.

--- src/main/java/Rivera/CEN4025C/Main.java
+++ src/main/java/Rivera/CEN4025C/Main.java
@@ -1,7 +1,20 @@
 package Rivera.CEN4025C;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("To-Do List");
+
+        List<String> todoList = new ArrayList<>();
+        //Declares `todoList` variable as an ArrayList
+        //Reference [1]
     }
 }
+
+
+/*
+References:
+[1] “List Interface in Java with Examples,” GeeksforGeeks, Oct. 27, 2016. https://www.geeksforgeeks.org/list-interface-java-examples/ (accessed Sep. 10, 2023).
+*/
